78fc17a616034b73db732e6696bc9aa5cca956b1
eddiesancui <eddiesan.cui@gmail.com>
2025-09-02 07:58:40 +0800

    working POM login logout automation

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -5,7 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 class DashboardPage:
     # Locator — assumes a heading or element visible upon login
     DASHBOARD_HEADER = (By.CSS_SELECTOR, ".oxd-topbar-header-title")
-    LOGOUT_BUTTON = (By.XPATH, "//p[@class='oxd-userdropdown-name']")
+    USERDROPDOWN_BUTTON = (By.XPATH, "//p[@class='oxd-userdropdown-name']")
+    LOGOUT_BUTTON_2 = (By.XPATH, "//a[normalize-space()='Logout']")
 
     def __init__(self, driver, timeout=10):
         self.driver = driver
@@ -16,6 +17,7 @@ class DashboardPage:
 
     def click_logout(self):
         # Example: clicking the dropdown and selecting logout
-        self.wait.until(EC.element_to_be_clickable(self.LOGOUT_BUTTON)).click()
+        self.wait.until(EC.element_to_be_clickable(self.USERDROPDOWN_BUTTON)).click()
+        self.wait.until(EC.element_to_be_clickable(self.LOGOUT_BUTTON_2)).click()
         from pages.logout_page import LogoutPage
         return LogoutPage(self.driver)
